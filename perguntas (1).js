dc08de17849cbefe8f98c47073397ba8b6451291
feliepgama <00001105364999sp@al.educacao.sp.gov.br>
2024-11-18 15:34:11 -0300

    Add files via upload

---
+++ perguntas (1).js
@@ -0,0 +1,56 @@
+criaCartao(
+    'Química',
+    'Quais são os conceitos básicos de Química?',
+    'Os conceitos básicos de Química são importantes para introduzir os estudos dessa Ciência, como os conceitos de matéria, energia, substância, corpo, objeto e sistema'
+)
+
+criaCartao(
+    'Química',
+    'Quais são os tipos de Química?',
+    'química orgânica, química inorgânica, físico-química e química analítica'
+
+)
+
+criaCartao(
+    'Química',
+    'Onde a química e usada',
+    'Assar um pão ou bolo, respirar, utilizar água sanitária nas roupas, queimar um combustível ou acender uma vela são alguns exemplos de fenômenos químicos. Há reações, mudança de cores, forma. Tudo praticamente envolve química. Até um beijo'
+)
+
+criaCartao(
+    'historia',
+    'Quem é o pai da história?',
+    'Heródoto'
+)
+
+criaCartao(
+    'historia',
+    'Para que serve a história',
+    'A História serve para se entender o desenvolvimento das sociedades e dos valores da humanidade '
+
+)
+
+criaCartao(
+    'historia',
+    'O que a história estuda?',
+    'História é a ciência responsável por estudar os acontecimentos passados'
+)
+
+criaCartao(
+    'Química',
+    'O que é soluto?',
+    'Soluto é aquele que é dissolvido em um solvente'
+)
+
+criaCartao(
+    'Química',
+    'O que é Química Orgânica?',
+    'A área da Química que estuda dos compostos do Carbono'
+
+)
+
+criaCartao(
+    'Química',
+    'Onde utilizamos a química geral?',
+    ', a Química Geral permite entender fenômenos naturais e processos industriais, sendo essencial para diversas aplicações em laboratórios e indústrias químicas.'
+)
